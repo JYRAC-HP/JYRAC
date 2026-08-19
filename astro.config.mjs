af70041b4f48cbae17d1a622df6685427a3f8d4b
Kombumori <96778966+FRLP@users.noreply.github.com>
2026-08-19 21:42:09 +0900

    fix: SSG -> SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jyrac.stki.org',
+  output: 'server',   // ← 追加
   adapter: cloudflare(),
   integrations: [sitemap()],
 });
